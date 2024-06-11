c5a3264ba51a39171bd8c6a9773a0711c7ac88f4
minhquan1313 <minhquan1313@gmail.com>
2024-06-11 12:23:17 +0700

    up

--- babel.config.js
+++ babel.config.js
@@ -5,6 +5,7 @@ module.exports = function (api) {
     plugins: [
       //
       "nativewind/babel",
+      "@babel/plugin-proposal-export-namespace-from",
       "react-native-reanimated/plugin",
     ],
   };
